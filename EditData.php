0a699bb95a860bd79c7ffe3b5d716e26993d0b0c
@chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2020-01-17 19:35:56 +0800

    updated

--- EditData.php
+++ EditData.php
@@ -80,6 +80,11 @@
                             </div>
                         </form>
                     </div>
+
+                    <div class="signup-image">
+                    <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+                    <!-- <a href="Login.php" class="signup-image-link">I am already member</a> -->
+                </div>
                     
                 </div>
             </div>
